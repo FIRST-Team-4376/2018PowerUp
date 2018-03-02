3d251441f522e2199f665444442edd00661a0a09
Burke Ramsey <birk5437@gmail.com>
2018-03-02 09:00:00 -0500

    auton drive across line

--- src/org/usfirst/frc/team4376/robot/commands/AutonLeft.java
+++ src/org/usfirst/frc/team4376/robot/commands/AutonLeft.java
@@ -1,18 +1,19 @@
 package org.usfirst.frc.team4376.robot.commands;
+import org.usfirst.frc.team4376.robot.Robot;
+
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutonLeft extends CommandGroup{
   public AutonLeft() {
-    String gameData;
-    gameData = DriverStation.getInstance().getGameSpecificMessage();
-    if(gameData.length() > 0){
-      if(gameData.charAt(0) == 'L'){
-        addSequential(new AutonPidDriveStraightCommand(160.5));
-        addSequential(new AutonPidTurnToAngleCommand(90.0));
-        addSequential(new AutonPidDriveStraightCommand(13.5));
-      } else {
-        addSequential(new AutonPidDriveStraightCommand(220));
-      }
-    }
+//    if(Robot.gameData.length() > 0){
+//      if(gameData.charAt(0) == 'L'){
+//        addSequential(new AutonPidDriveStraightCommand(160.5));
+//        addSequential(new AutonPidTurnToAngleCommand(90.0));
+//        addSequential(new AutonPidDriveStraightCommand(13.5));
+//      } else {
+//        addSequential(new AutonPidDriveStraightCommand(220));
+//      }
+//    }
+	  addSequential(new AutonPidDriveStraightCommand(160));
   }
 }
