61504cb216c4cfd1a340a66a0a50818a0b5989b5
Burke Ramsey <birk5437@gmail.com>
2018-03-02 14:28:54 -0500

    dont calibrate gyron in auton init

--- src/org/usfirst/frc/team4376/robot/commands/AutonDriveStraightTimeBased.java
+++ src/org/usfirst/frc/team4376/robot/commands/AutonDriveStraightTimeBased.java
@@ -22,22 +22,22 @@ public class AutonDriveStraightTimeBased extends Command {
 	// Called just before this Command runs the first time
 	@Override
 	protected void initialize() {
+		startingTimerValue = timer.get();
+		startingGyroAngle = Robot.gyro.getAngleZ();
 		timer = new Timer();
 		timer.reset();
 		timer.start();
-		startingTimerValue = timer.get();
-		Robot.gyro.calibrate();
-		Robot.gyro.reset();
-		startingGyroAngle = Robot.gyro.getAngleZ();
 	}
 
 	// Called repeatedly when this Command is scheduled to run
 	@Override
 	protected void execute() {
 		double timerValue = Math.abs(timer.get()) - Math.abs(startingTimerValue) ;
-		System.out.println("AUTON - " + timerValue);
+		System.out.println("AUTON STARTING TIMER VALUE " + startingTimerValue);
+		System.out.println("AUTON TIMER dot GET " + timer.get());
+		System.out.println("AUTON TIMER VALUE " + timerValue);
 		if(timerValue > 0.0 && timerValue <= 6.0){
-			Robot.chassis.driveStraightAtAngle(startingGyroAngle, .5);
+			Robot.chassis.driveStraightAtAngle(startingGyroAngle, .7);
 		}
 		
 	}
