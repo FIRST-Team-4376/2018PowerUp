3095667c164027c00cae734edad71f012dac6568
Burke Ramsey <birk5437@gmail.com>
2018-03-17 13:27:46 -0400

    added EncoderSafetyTimer

--- src/org/usfirst/frc/team4376/robot/commands/AutonPidDriveStraightCommand.java
+++ src/org/usfirst/frc/team4376/robot/commands/AutonPidDriveStraightCommand.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team4376.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
 import org.usfirst.frc.team4376.robot.Robot;
@@ -9,6 +10,7 @@ import org.usfirst.frc.team4376.robot.Robot;
  */
 public class AutonPidDriveStraightCommand extends Command {
 	double speed; 
+	Timer t;
 	double targetDistance; 
 	double marginOfError = 10;
 	public AutonPidDriveStraightCommand(double distanceInches) {
@@ -28,6 +30,8 @@ public class AutonPidDriveStraightCommand extends Command {
 	// Called just before this Command runs the first time
 	@Override
 	protected void initialize() {
+		t.reset();
+		t.start();
 		Robot.driveMotorR.reset();
 		Robot.gyro.reset();
 		Robot.pidDriveStraight.setOutputRange(speed * -1, speed);
@@ -47,7 +51,10 @@ public class AutonPidDriveStraightCommand extends Command {
 		if ((targetDistance - marginOfError) < Robot.driveMotorR.get() &&
 				Robot.driveMotorR.get() < (targetDistance + marginOfError)){
 			return true;
-		} else { return false; }
+		} else if(t.get() > 4.0){
+			return true;
+		}
+		else { return false; }
 	}
 
 	// Called once after isFinished returns true
